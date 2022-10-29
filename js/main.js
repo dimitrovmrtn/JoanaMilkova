77003abea008b33cbca5a35201a9e19fc22a60e3
Martin Dimitrov <Martin Dimitrov@MDIMITROVET>
2022-10-29 03:42:20 +0300

    deleted some commented code

--- js/main.js
+++ js/main.js
@@ -4,48 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
       document.querySelector(".toggler").click()
     )
   );
-  // document
-  //   .querySelector(".toggler")
-  //   .addEventListener("click", () =>
-  //     document
-  //       .querySelector(".dropdown-content")
-  //       .classList.remove("mobileDropDown")
-  //   );
-  // document.querySelector(".dropdown-content").firstChild.remove();
-  // document.querySelector("#dropdown").addEventListener("mouseover", () => {
-  //   document.querySelector(".dropdown-content").style.display = "block";
-  //   document.querySelector(".dropdown-content").style.top = `${
-  //     145 + window.pageYOffset
-  //   }px`;
-  // });
-  // document.querySelector(".fa-angle-down").addEventListener("click", () => {
-  //   document.querySelector(".collapsed-content").classList.toggle("show");
-  // });
-  // document.querySelectorAll(".collapsible-question").forEach((el, index) => {
-  //   el.addEventListener("click", () => {
-  //     document.querySelector(`#answer-${index}`).classList.toggle("show");
-  //   });
-  // });
-  // document
-  //   .querySelector(".dropdown-content")
-  //   .addEventListener(
-  //     "mouseover",
-  //     () =>
-  //       (document.querySelector(".dropdown-content").style.display = "block")
-  //   );
-  // document
-  //   .querySelector("#dropdown")
-  //   .addEventListener(
-  //     "mouseout",
-  //     () => (document.querySelector(".dropdown-content").style.display = "none")
-  //   );
-  // document
-  //   .querySelector(".dropdown-content")
-  //   .addEventListener(
-  //     "mouseout",
-  //     () => (document.querySelector(".dropdown-content").style.display = "none")
-  //   );
-  // let links = [...document.getElementsByTagName("a")];
 });
 
 const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
